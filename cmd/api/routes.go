8bde12786366232efb8ae39871fd02013ae800d8
giorgiomiraglia <grgmiraglia@gmail.com>
2025-10-30 20:56:30 -0300

    Setting custom error handlers for httprouter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc("GET", "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc("GET", "/v1/movies/:id", app.showMoviesHandler)
-
 	router.HandlerFunc("POST", "/v1/movies", app.createMoviesHandler)
 
 	return router
