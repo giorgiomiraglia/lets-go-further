92c973c1c07d41faad93e01f5e8aacb2012354ca
giorgiomiraglia <grgmiraglia@gmail.com>
2025-10-30 22:30:03 -0300

    Changing type of 'Runtime' in input struct

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,13 +10,13 @@ import (
 func (app *application) createMoviesHandler(w http.ResponseWriter, r *http.Request) {
 
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
-	err := app.readJSON(r, &input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
