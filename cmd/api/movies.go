32d78f8f3c78d60cc87048cb50764abe4e82507d
giorgiomiraglia <grgmiraglia@gmail.com>
2025-10-30 21:57:23 -0300

    Using readJSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -8,7 +8,21 @@ import (
 )
 
 func (app *application) createMoviesHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Creates a new movie\n")
+
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMoviesHandler(w http.ResponseWriter, r *http.Request) {
