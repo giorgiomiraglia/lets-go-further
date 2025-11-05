d37cc0d63e3465dffc57828b0b9f457af06c1f60
giorgiomiraglia <grgmiraglia@gmail.com>
2025-11-04 21:10:16 -0300

    Implementing CreateMovie logic

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -38,7 +38,19 @@ func (app *application) createMoviesHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	err = app.models.Movies.Insert(m)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", m.ID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": m}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) showMoviesHandler(w http.ResponseWriter, r *http.Request) {
