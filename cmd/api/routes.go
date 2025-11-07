37a0587704abdbbf03562acea3e157d8fe5bbe57
giorgiomiraglia <grgmiraglia@gmail.com>
2025-11-06 22:02:42 -0300

    Adding the GET movies route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,6 +14,7 @@ func (app *application) routes() *httprouter.Router {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandlers)
 
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 
