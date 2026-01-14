266776689fb6915ed4526911253bc723cd843d1a
GmiR <138176697+giorgiomiraglia@users.noreply.github.com>
2026-01-14 12:16:14 -0300

    Adding panic recover and rate limiting

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -22,5 +22,5 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
-	return app.recoverPanic(router)
+	return app.recoverPanic(rateLimit(router))
 }
