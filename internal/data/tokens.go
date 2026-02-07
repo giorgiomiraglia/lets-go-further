f6401ea430c09a826f843c14c6abd3d8d970e337
giorgiomiraglia <grgmiraglia@gmail.com>
2026-02-07 00:49:11 -0300

    Changing from value to reference

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -54,6 +54,7 @@ func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error
 }
 
 func (m *TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+
 	token, err := generateToken(userID, ttl, scope)
 	if err != nil {
 		return nil, err
