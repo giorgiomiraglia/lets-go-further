9998fa7c4d140e1d0d72eb85d91346ae1c847303
giorgiomiraglia <grgmiraglia@gmail.com>
2026-02-08 14:09:14 -0300

    Implementing authentication token generation

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"errors"
 	"greenlight/internal/data"
 	"greenlight/internal/validator"
@@ -9,7 +8,7 @@ import (
 	"time"
 )
 
-func (app *application) createActivationToken(w http.ResponseWriter, r *http.Request) {
+func (app *application) createActivationTokenHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Email string `json:"email"`
 	}
@@ -29,7 +28,7 @@ func (app *application) createActivationToken(w http.ResponseWriter, r *http.Req
 	user, err := app.models.Users.GetByEmail(input.Email)
 	if err != nil {
 		switch {
-		case errors.Is(err, sql.ErrNoRows):
+		case errors.Is(err, data.ErrRecordNotFound):
 			v.AddError("email", "no matching email address found")
 			app.failedValidationResponse(w, r, v.Errors)
 		default:
@@ -56,6 +55,61 @@ func (app *application) createActivationToken(w http.ResponseWriter, r *http.Req
 		envelope{"token": token.PlainText},
 		nil,
 	)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	data.ValidateEmail(v, input.Email)
+	data.ValidatePassword(v, input.Password)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.invalidCredentialsResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	match, err := user.Password.Matches(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	if !match {
+		app.invalidCredentialsResponse(w, r)
+		return
+	}
+
+	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeAuthentication)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
