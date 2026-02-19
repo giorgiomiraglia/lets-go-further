409bb8b56f38addbe806f7299c0950f22266144a
giorgiomiraglia <grgmiraglia@gmail.com>
2026-02-18 22:34:51 -0300

    Passing the database dsn to makefile and .envrc

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -61,7 +61,7 @@ func main() {
 	// App configs
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost:5435/greenlight?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "dm-max-idle-time", "15m", "PostgreSQL max connection idle time")
